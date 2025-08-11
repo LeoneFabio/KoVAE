1e2e28e3ce4767cf0586244535a2bc4df289b04f
LEONE FABIO s330500 <s330500@studenti.polito.it>
2025-08-11 18:44:38 +0200

    Refactor charge mode filtering in preprocess_dataset to handle NaN values

--- utils/utils_data.py
+++ utils/utils_data.py
@@ -224,7 +224,7 @@ def preprocess_dataset(path, data_name, event=None, charge_mode=None):
 
     if charge_mode is not None:
         if charge_mode == '0':
-            df = df[df['charge_mode'].astype(str).str.strip().str.lower().str.contains(charge_mode.lower()) | df['charge_mode'].astype(str).str.strip().str.lower().str.contains('nan')]
+            df = df[df['charge_mode'].isna()]
         else:
             df = df[df['charge_mode'].astype(str).str.strip().str.lower().str.contains(charge_mode.lower())]
     ###################################################################################
