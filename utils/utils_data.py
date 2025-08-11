4e88d860fb9eb34510dd11b899f7d73b93249ea8
LEONE FABIO s330500 <s330500@studenti.polito.it>
2025-08-11 19:00:47 +0200

    Add debug prints to preprocess_dataset for charge_mode filtering

--- utils/utils_data.py
+++ utils/utils_data.py
@@ -222,11 +222,15 @@ def preprocess_dataset(path, data_name, event=None, charge_mode=None):
     if event is not None:
         df = df[df['event'].str.strip().str.lower() == event.lower()]
 
+    print('before filtering')
+    print(df.shape)
     if charge_mode is not None:
         if charge_mode == '0':
             df = df[df['charge_mode'].isna()]
         else:
             df = df[df['charge_mode'].astype(str).str.strip().str.lower().str.contains(charge_mode.lower())]
+    print('after filtering')
+    print(df.shape)
     ###################################################################################
     
     
